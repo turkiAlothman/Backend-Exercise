ce7cb6a7e85f0504d89a9cf44d3992d275673341
Turki Alothman <turki@abyancapital.sa>
2024-02-18 07:19:43 +0300

    Routes and Controllers

---
+++ app/Http/Controllers/PropertyController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Property;
+use http\Env\Response;
+use Illuminate\Http\Request;
+
+class PropertyController extends Controller
+{
+    public static function index(Request $request , Property $Property){
+        return \response()->json($Property->toArray());
+}
+    public static function getProperties(Request $request){
+        $QueryParameters = $request->query();
+        $Properties = Property::getAll($QueryParameters);
+        return \response()->json($Properties->toArray());
+    }
+    public static function delete(Request $request, Property $Property){
+        # no need to check whether the user exist or not . automatically will response with 404.
+        $Property->delete();
+        return \response()->json(["message"=>"Property with id deleted successfully"]);
+    }
+    public static function create(Request $request){
+        Property::manage($request->all());
+        return \response()->json(["message"=>"Property created successfully"]);
+    }
+    public static function update(Request $request , Property $Property){
+        Property::manage($request->all(),$Property->id);
+        return \response()->json(["message"=>"Property updated successfully"]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\PropertyController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get("/properties/{Property}",[PropertyController::class,"index"]);
+Route::get("/properties",[PropertyController::class,"getProperties"]);
+Route::delete("/properties/delete/{Property}",[PropertyController::class,"delete"]);
+Route::post("/properties/create",[PropertyController::class,"create"]);
+Route::patch("/properties/update/{Property}",[PropertyController::class,"update"]);
